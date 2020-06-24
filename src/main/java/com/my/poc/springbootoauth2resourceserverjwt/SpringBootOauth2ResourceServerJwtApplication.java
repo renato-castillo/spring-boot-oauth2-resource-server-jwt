10cb10a78dbfe3727715d582eb17df628a899617
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-24 00:12:22 -0400

    Spring boot app initial skeleton

---
+++ src/main/java/com/my/poc/springbootoauth2resourceserverjwt/SpringBootOauth2ResourceServerJwtApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.springbootoauth2resourceserverjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2ResourceServerJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2ResourceServerJwtApplication.class, args);
+	}
+
+}
